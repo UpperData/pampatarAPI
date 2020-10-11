a236e2e6fd401998e5943c9f330a9453dcf1b7c1
root <root@upperdata-server.cantv.net>
2020-10-11 02:20:56 -0400

    https include

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan =require('morgan');
 const cors =require('cors');
 var helmet = require('helmet');
+const https = require("https");
+require('dotenv').config();
+  fs = require("fs");
+
 //setting
 var app=express(); // Express Initialize
 app.set('port',process.env.PORT || 4094 ); // comunication port
@@ -31,9 +35,16 @@ app.use(require('./routes/back/warehouse.route'));
 app.use(require('./routes/back/envoice.route'));
 app.use(require('./routes/back/panel.route'));
 
+const options = {
+	key: fs.readFileSync(process.env.SSL_CERTIFICATE_KEY),
+	cert: fs.readFileSync(process.env.SSL_CERTIFICATE), // these paths might differ for you, make sure to copy from the certbot output
+	dhparam: fs.readFileSync(process.env.DH_PARAM)
+  };
+
 // Start Server
 app.listen(app.get('port'),function(){
 
 	console.log('Pampatar is working in port:', app.get('port'));
 	
 });
+https.createServer(options, app).listen(8080);
