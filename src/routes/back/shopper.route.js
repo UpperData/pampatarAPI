2c0240d85a55ac763be2f797fe4acfe65b44de04
root <root@upperdata-server.cantv.net>
2020-10-13 00:15:08 -0400

    get shopRequestsByStatus

--- src/routes/back/shopper.route.js
+++ src/routes/back/shopper.route.js
@@ -6,6 +6,6 @@ const authorization =require('../../controllers/middleware/authorization.ctrl');
 
 router.post('/shop-request',authorization.requireRole([{"id":6}]),shopper.shopRequest) // Solicitud de creación de tienda
 router.get('/shop-request/shopExists/:shopName',shopper.validateShop) // valida si existe la tienda
-router.get('/shop-request/get/:status',authorization.requireRole([{"id":7}]),shopper.getShopRequestByStatus) // Postulaciones por estatus
+router.get('/shop-request/get/:status',shopper.getShopRequestByStatus) // Postulaciones por estatus
 
 module.exports=router;
