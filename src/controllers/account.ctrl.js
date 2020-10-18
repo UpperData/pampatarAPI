6928632e04673b067ecf65200d36766a89141fa9
root <root@upperdata-server.cantv.net>
2020-10-18 11:39:30 -0400

    Login token

--- src/controllers/account.ctrl.js
+++ src/controllers/account.ctrl.js
@@ -490,7 +490,7 @@ async function loginToken(req,res){
 		.then(async function validTk(){
 			await generals.getShopId(token)
 			.then(async function(getShop){
-				console.log("Tiendas: "+getShop.length)
+				console.log("Tiendas: "+getShop)
 				if(getShop){
 					res.josn({"data":{"account":payload.account,"role":payload.role, "people":payload.people,"shop":getShop}}	)			
 				}else{
