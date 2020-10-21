9ec923adfcdc431f4b9fbec217c97187bc4157f0
root <root@upperdata-server.cantv.net>
2020-10-20 22:47:04 -0400

    Login token

--- src/controllers/account.ctrl.js
+++ src/controllers/account.ctrl.js
@@ -489,8 +489,7 @@ async function loginToken(req,res){
 		.then(async function(rsCurrentAccount){
 			await generals.getShopId(token)
 			.then(async function(getShop){
-				console.log("Role: "+rsCurrentAccount['data'].role)
-				res.send(rsCurrentAccount['data']['role'].id)
+				console.log("Role: "+rsCurrentAccount['data']['role']['rows'].id)				
 				if(getShop){
 					res.json({"data":{"result":true,"message":"Usted a iniciado sesión como "+rsCurrentAccount['data'].account.email,
 						"account":{ "id": rsCurrentAccount['data'].account.id,"name":rsCurrentAccount['data'].account.name,"email":rsCurrentAccount['data'].account.email},
