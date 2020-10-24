f48e8931399c1f1772aa6c7b0f08734ec894f391
root <root@upperdata-server.cantv.net>
2020-10-23 22:42:51 -0400

    token login fixing

--- src/routes/back/account.route.js
+++ src/routes/back/account.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //router.get('/setting/account/list',authorization.requireRole({"id":5}),getAll); // Listar Cuentas de Usuario
 router.get('/setting/account/:id',account.getOne); // Buscar Usuario
 router.put('/setting/updatePass/',account.changePassword); // Buscar Usuario
-router.get('/boLogin/:token',account.loginToken); // autenticación por token
+router.get('/boLogin/:token',authorization.requireRole([{"id":5}]),account.loginToken); // autenticación por token
 router.post('/login/back',account.loginBackoffice); // Autenticación de usuario en backoffice
 
 //router.get('/setting/account/list',authorization.requireRole({"id":5}),getAll); // Listar Cuentas de Usuario
