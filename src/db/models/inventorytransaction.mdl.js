5c547921134c2a4d05e52bb724544583bd4ec6ef
root <root@upperdata-server.cantv.net>
2020-12-17 00:06:49 -0400

    fix inventory lot

---
+++ src/db/models/inventorytransaction.mdl.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const inventoryTransaction = sequelize.define('inventoryTransaction', {
+    type: {
+      type:DataTypes.STRING,      
+      allowNull:false,
+      validate:{
+        isIn:[['out','in']]
+      }
+    },
+    quantity: {
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+    },
+    inventoryId: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+    },
+    AccountId: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+    }
+  }, {});
+  inventoryTransaction.associate = function(models) {
+    // associations can be defined here
+    inventoryTransaction.belongsTo(models.inventory);
+    inventoryTransaction.belongsTo(models.Account);
+  };
+  return inventoryTransaction;
+};
