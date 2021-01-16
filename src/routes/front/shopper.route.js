94e25bc2b01ef881cfecf9f4ff4501d23d9959a4
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-01-16 13:25:17 -0400

    fixed shoprequest view

--- src/routes/front/shopper.route.js
+++ src/routes/front/shopper.route.js
@@ -2,8 +2,9 @@ const express=require('express');
 const router=express.Router();
 const shopper=require('../../controllers/shopper.ctrl');
 const authorization =require('../../controllers/middleware/authorization.ctrl');
+const forceBrute=require('../../controllers/middleware/notBrute.ctrl');
 
-router.get('/shop-request/vIew/:id',authorization.requireRole(([{"id":5}])),shopper.shopRequestView) // Solicitud de creación de tienda
+router.get('/shop-request/vIew/:id',forceBrute.notBruteSecure,authorization.requireRole(([{"id":5}])),shopper.shopRequestView) // Solicitud de creación de tienda
 //router.get('/shop-request/validBrand',home.existBrand) // valida si existe la marca
 
 module.exports=router;
