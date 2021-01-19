79ce6933f998aaa948c6d40ffe8b7482787bfadc
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-01-19 13:30:20 -0400

    fixed get shopRequest view

--- src/controllers/generals.ctrl.js
+++ src/controllers/generals.ctrl.js
@@ -5,12 +5,12 @@ var jwt=require('jwt-simple');
 
 async function currentAccount(token){
 	//console.log(token);	
-	var  payload= await jwt.decode(token,process.env.JWT_SECRET);
-	if(payload){
+	try{
+		var  payload= await jwt.decode(token,process.env.JWT_SECRET);
 		const dataToken={"data":{"account":payload.account,"role":payload.role, "people":payload.people,"shop":payload.shop}}
 		return dataToken;  
-	}else{
-		return null
+	}catch(error)	{
+		return null;
 	}
 	
 	//console.log(dataToken);
