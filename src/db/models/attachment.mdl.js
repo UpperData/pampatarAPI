9dda4da8dadb65eedea9929048b6a2af909ef106
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-01-27 14:00:47 -0400

    attachmentMDL - Fix

--- src/db/models/attachment.mdl.js
+++ src/db/models/attachment.mdl.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const attachment = sequelize.define('attachment', {
     data: 
-    {type:DataTypes.BLOB('long'),
+    {type:DataTypes.STRING,
     allowNull:false,
     validate:{     
       notEmpty:true        
