3083aa6279f7317f6985129243579abb6056141f
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-02-25 15:04:38 -0400

    bids create an view by token

--- src/routes/back/bid.route.js
+++ src/routes/back/bid.route.js
@@ -5,7 +5,7 @@ const bid =require('../../controllers/bid.ctrl');
 
 
 //router.get('/artisan/bid/:id',authorization.requireRole([{"id":5},{"id":7}]),bid.getOne); // Get One 
-router.get('/artisan/bid/',authorization.requireRole([{"id":5},{"id":7}]),bid.getAllMine); // Get all from artisan
+router.post('/seller/puBLIctIons/bid/aDd',authorization.requireRole([{"id":5}]),bid.addBid); // Get all from artisan
 //router.post('/setting/artisan/bid/',authorization.requireRole([{"id":5},{"id":7}]),bid.add); // Add One 
 //router.put('/setting/artisan/bid/:id',authorization.requireRole([{"id":5},{"id":7}]),bid.edit); // edit
 module.exports=router;
