621262d7bad8531b789cb030b40d9ee6b68b668f
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-03-24 14:35:03 -0400

    fix shopper profile edit

--- src/controllers/shopper.ctrl.js
+++ src/controllers/shopper.ctrl.js
@@ -21,128 +21,131 @@ async  function shopRequest(req,res){
 	var horaActual=y+'-'+mm+'-'+d+':'+h+':'+m+':'+s;
 	req.body.status=[{"id":1,"name":"En Evaluación","date":horaActual}]	
 	const token = req.header('Authorization').replace('Bearer ', '')
-
-	//var  payload= await jwt.decode(token,process.env.JWT_SECRET);	
-	var payload=generals.currentAccount(token)
-	const AccountId=payload['data']['account'].id;		
-	const {document,status,firstName,lastName,
-	phone,marca,storeType,startActivity,isStore,
-	descShop,salesChannels,affirmations,employees,
-	birthDate,genderId,nationalityId}=req.body;
-	
-	return await model.Account.findAndCountAll({where:{id:AccountId,confirmStatus:true,statusId:1},
-	include:[{model:model.People}], transaction: t })
-	.then(async function (rsAccount) {
-		if (rsAccount.count>0) {
-			return await model.People.findOrCreate({where:{id:rsAccount['rows'][0].id},	transaction:t,
-				defaults: {document,firstName,lastName,birthDate,genderId,nationalityId,statusId:1}}).
-				spread(async function(rsPeople, created) {				
-				if(created){
-					await model.Account.update({peopleId:rsPeople.id},{where:{id:rsAccount['rows'][0].id}, transaction: t})
-				}							
-				return await model.shopRequest.findOrCreate({where: {AccountId} ,transaction: t, 
-					defaults: {status,phone,marca,storeType,startActivity,isStore,descShop,
-								salesChannels,affirmations,employees}})
-					.spread(async function(sRequest, created) {
-						if (created) {
-							const type="shopRequestsView"	  //tipo de TOken (accountId,roles,shops,peoples,type)
-							hash=await servToken.newToken(rsAccount,{"id":5,"name":"Comprador"},{sRequest},rsPeople,type) //generar Token 
-							var link=process.env.HOST_FRONT+"viewRequest/"+hash; // crea link Para ver Postulación
-							var link2=process.env.HOST_FRONT+"viewRequest/"+hash; // crea link Para ver
-							var mailsendShoper= mail.sendEmail({
-							"from":'"Pampatar" <'+process.env.EMAIL_ADMIN+'>', 
-							"to":rsAccount['rows'][0].email,
-							"subject": '.:Tienda Pampatar - Postulación:.',
-							"text":"¡Enhorabuena!, Estas aun paso de formar parte de Pampatar",							
-							"html": `<!doctype html>
-							<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar.png" alt="Logo Pampatar.cl" width="250" height="97" style="display:block; margin-left:auto; margin-right:auto; margin-top: 25px; margin-bottom:25px"> 
-							<hr style="width: 420; height: 1; background-color:#99999A;">
-							<link rel="stylesheet" href="http://192.99.250.22/pampatar/assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
-						
-							<div  align="center">
-								<h2 style="font-family:sans-serif; color:#ff4338;" >¡Enhorabuena!</h2>
-								<p style="font-family:sans-serif; font-size: 19px;" >Ha solicitado crear una nueva tienda en Pampatar</p>
+	if(token){
+		//var  payload= await jwt.decode(token,process.env.JWT_SECRET);	
+		var payload=generals.currentAccount(token)
+		const AccountId=payload['data']['account'].id;		
+		const {document,status,firstName,lastName,
+		phone,marca,storeType,startActivity,isStore,
+		descShop,salesChannels,affirmations,employees,
+		birthDate,genderId,nationalityId}=req.body;
+		
+		return await model.Account.findAndCountAll({where:{id:AccountId,confirmStatus:true,statusId:1},
+		include:[{model:model.People}], transaction: t })
+		.then(async function (rsAccount) {
+			if (rsAccount.count>0) {
+				return await model.People.findOrCreate({where:{id:rsAccount['rows'][0].id},	transaction:t,
+					defaults: {document,firstName,lastName,birthDate,genderId,nationalityId,statusId:1}}).
+					spread(async function(rsPeople, created) {				
+					if(created){
+						await model.Account.update({peopleId:rsPeople.id},{where:{id:rsAccount['rows'][0].id}, transaction: t})
+					}							
+					return await model.shopRequest.findOrCreate({where: {AccountId} ,transaction: t, 
+						defaults: {status,phone,marca,storeType,startActivity,isStore,descShop,
+									salesChannels,affirmations,employees}})
+						.spread(async function(sRequest, created) {
+							if (created) {
+								const type="shopRequestsView"	  //tipo de TOken (accountId,roles,shops,peoples,type)
+								hash=await servToken.newToken(rsAccount,{"id":5,"name":"Comprador"},{sRequest},rsPeople,type) //generar Token 
+								var link=process.env.HOST_FRONT+"viewRequest/"+hash; // crea link Para ver Postulación
+								var link2=process.env.HOST_FRONT+"viewRequest/"+hash; // crea link Para ver
+								var mailsendShoper= mail.sendEmail({
+								"from":'"Pampatar" <'+process.env.EMAIL_ADMIN+'>', 
+								"to":rsAccount['rows'][0].email,
+								"subject": '.:Tienda Pampatar - Postulación:.',
+								"text":"¡Enhorabuena!, Estas aun paso de formar parte de Pampatar",							
+								"html": `<!doctype html>
+								<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar.png" alt="Logo Pampatar.cl" width="250" height="97" style="display:block; margin-left:auto; margin-right:auto; margin-top: 25px; margin-bottom:25px"> 
+								<hr style="width: 420; height: 1; background-color:#99999A;">
+								<link rel="stylesheet" href="http://192.99.250.22/pampatar/assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
 							
-							<a href="`+link+`"><input class="btn btn-primary btn-lg" style="font-size:16px; background-color: #ff4338;  border-radius: 10px 10px 10px 10px; color: white;" type="button" value="Revisar formulario"></a>
-							</div>
-							<br><br><br>
-								<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar-sin-avion.png" alt="Logo Pampatar.cl" width="120" height="58" style="display:block; margin-left:auto; margin-right:auto; margin-top: auto; margin-bottom:auto">
-								<br>
-								<div  style="margin-left:auto;font-family:sans-serif; margin-right:auto; margin-top:15px; font-size: 11px;">
-									<p align="center">	
-										<a href="#">Quiénes somos</a> | <a href="#">Políticas de privacidad</a> | <a href="#">Términos y condiciones</a> | <a href="#">Preguntas frecuentes</a> 
-									</p>					
+								<div  align="center">
+									<h2 style="font-family:sans-serif; color:#ff4338;" >¡Enhorabuena!</h2>
+									<p style="font-family:sans-serif; font-size: 19px;" >Ha solicitado crear una nueva tienda en Pampatar</p>
+								
+								<a href="`+link+`"><input class="btn btn-primary btn-lg" style="font-size:16px; background-color: #ff4338;  border-radius: 10px 10px 10px 10px; color: white;" type="button" value="Revisar formulario"></a>
+								</div>
+								<br><br><br>
+									<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar-sin-avion.png" alt="Logo Pampatar.cl" width="120" height="58" style="display:block; margin-left:auto; margin-right:auto; margin-top: auto; margin-bottom:auto">
+									<br>
+									<div  style="margin-left:auto;font-family:sans-serif; margin-right:auto; margin-top:15px; font-size: 11px;">
+										<p align="center">	
+											<a href="#">Quiénes somos</a> | <a href="#">Políticas de privacidad</a> | <a href="#">Términos y condiciones</a> | <a href="#">Preguntas frecuentes</a> 
+										</p>					
+								
+										<p  align="center" >
+										info@estudiopampatar.cl
+												Santiago de Chile, Rinconada el salto N°925, Huechuraba +56 9 6831972
+										</p>
+									</div>`
+								},{ transaction: t })	
+		
+								var mailsendAdmin= mail.sendEmail({
+								"from":'"Pampatar" <'+process.env.EMAIL_ADMIN+'>', 
+								"to":process.env.EMAIL_ADMIN_SYS,
+								"subject": '.:Nueva Postulación:.',
+								"text":"Hay un nuevo postulado",							
+								"html": `<!doctype html>
+								<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar.png" alt="Logo Pampatar.cl" width="250" height="97" style="display:block; margin-left:auto; margin-right:auto; margin-top: 25px; margin-bottom:25px"> 
+								<hr style="width: 420; height: 1; background-color:#99999A;">
+								<link rel="stylesheet" href="http://192.99.250.22/pampatar/assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
 							
-									<p  align="center" >
-									info@estudiopampatar.cl
-											Santiago de Chile, Rinconada el salto N°925, Huechuraba +56 9 6831972
-									</p>
-								</div>`
-							},{ transaction: t })	
-	
-							var mailsendAdmin= mail.sendEmail({
-							"from":'"Pampatar" <'+process.env.EMAIL_ADMIN+'>', 
-							"to":process.env.EMAIL_ADMIN_SYS,
-							"subject": '.:Nueva Postulación:.',
-							"text":"Hay un nuevo postulado",							
-							"html": `<!doctype html>
-							<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar.png" alt="Logo Pampatar.cl" width="250" height="97" style="display:block; margin-left:auto; margin-right:auto; margin-top: 25px; margin-bottom:25px"> 
-							<hr style="width: 420; height: 1; background-color:#99999A;">
-							<link rel="stylesheet" href="http://192.99.250.22/pampatar/assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
-						
-							<div  align="center">
-								<h2 style="font-family:sans-serif; color:#ff4338;" >¡Nueva Postulación!</h2>
-								<p style="font-family:sans-serif; font-size: 19px;" > ` + firstName +` `+ lastName + ` ha solicitado crear una nueva tienda en Pampatar</p>
+								<div  align="center">
+									<h2 style="font-family:sans-serif; color:#ff4338;" >¡Nueva Postulación!</h2>
+									<p style="font-family:sans-serif; font-size: 19px;" > ` + firstName +` `+ lastName + ` ha solicitado crear una nueva tienda en Pampatar</p>
+								
+									<a href="`+link2+`"><input class="btn btn-primary btn-lg" style="font-size:16px; background-color: #ff4338;  border-radius: 10px 10px 10px 10px; color: white;" type="button" value="Revisar formulario"></a>
+								</div>
+								<br>						
+									<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar-sin-avion.png" alt="Logo Pampatar.cl" width="120" height="58" style="display:block; margin-left:auto; margin-right:auto; margin-top: auto; margin-bottom:auto">
+									<br>
+									<div  style="margin-left:auto;font-family:sans-serif; margin-right:auto; margin-top:15px; font-size: 11px;">
+										<p align="center">	
+											<a href="https://www.pampatar.cl/quienes-somos/">Quiénes somos</a> | <a href="https://www.pampatar.cl/legal/">Políticas de privacidad</a> | <a href="https://www.pampatar.cl/preguntas-frecuentes/">Términos y condiciones</a> | <a href="https://www.pampatar.cl/preguntas-frecuentes/">Preguntas frecuentes</a> 
+										</p>					
+								
+										<p  align="center" >
+										info@estudiopampatar.cl
+												Santiago de Chile, Rinconada el salto N°925, Huechuraba +56 9 6831972
+										</p>
+									</div>`
+								},{ transaction: t })
+								if(mailsendAdmin && mailsendShoper)	{
+									await t.commit();
+									res.json({"data":{"result":true,"message":"Postulación registrada Satisfactoriamente"}})										
+								}else{
+									await t.rollback()
+									res.json({"data":{"result":false,"message":"Algo salió mal tratando de enviar Correo Electrónico"}})										
+								}									
+							}else {
+								await t.rollback();
+								console.error();									
+								res.json({"data":{"result":false,"message":"Ya usted posee un postulación en tramite con el número: "+sRequest.id}})
+							}        
+						}).catch (async function(err) {
+								console.log(err);
+								await t.rollback();
+								res.json({"data":{"result":false,"message":"Algo salió mal, no se pudo procesar su postulación"}})
 							
-								<a href="`+link2+`"><input class="btn btn-primary btn-lg" style="font-size:16px; background-color: #ff4338;  border-radius: 10px 10px 10px 10px; color: white;" type="button" value="Revisar formulario"></a>
-							</div>
-							<br>						
-								<img src="http://192.99.250.22/pampatar/assets/images/logo-pampatar-sin-avion.png" alt="Logo Pampatar.cl" width="120" height="58" style="display:block; margin-left:auto; margin-right:auto; margin-top: auto; margin-bottom:auto">
-								<br>
-								<div  style="margin-left:auto;font-family:sans-serif; margin-right:auto; margin-top:15px; font-size: 11px;">
-									<p align="center">	
-										<a href="https://www.pampatar.cl/quienes-somos/">Quiénes somos</a> | <a href="https://www.pampatar.cl/legal/">Políticas de privacidad</a> | <a href="https://www.pampatar.cl/preguntas-frecuentes/">Términos y condiciones</a> | <a href="https://www.pampatar.cl/preguntas-frecuentes/">Preguntas frecuentes</a> 
-									</p>					
-							
-									<p  align="center" >
-									info@estudiopampatar.cl
-											Santiago de Chile, Rinconada el salto N°925, Huechuraba +56 9 6831972
-									</p>
-								</div>`
-							},{ transaction: t })
-							if(mailsendAdmin && mailsendShoper)	{
-								await t.commit();
-								res.json({"data":{"result":true,"message":"Postulación registrada Satisfactoriamente"}})										
-							}else{
-								await t.rollback()
-								res.json({"data":{"result":false,"message":"Algo salió mal tratando de enviar Correo Electrónico"}})										
-							}									
-						}else {
-							await t.rollback();
-							console.error();									
-							res.json({"data":{"result":false,"message":"Ya usted posee un postulación en tramite con el número: "+sRequest.id}})
-						}        
-					}).catch (async function(err) {
-							console.log(err);
-							await t.rollback();
-							res.json({"data":{"result":false,"message":"Algo salió mal, no se pudo procesar su postulación"}})
-						
-					})
-			}).catch(async function(error){
+						})
+				}).catch(async function(error){
+					await t.rollback();
+					console.log(error);							
+					res.json({"data":{"result":false,"message":"Algo salió mal, actualizando cuenta de usuario"}})
+				})
+			}else{
 				await t.rollback();
 				console.log(error);							
-				res.json({"data":{"result":false,"message":"Algo salió mal, actualizando cuenta de usuario"}})
-			})
-		}else{
+				res.json({"data":{"result":false,"message":"Cuenta invalida"}})
+			}
+		}).catch(async function(error){
 			await t.rollback();
 			console.log(error);							
-			res.json({"data":{"result":false,"message":"Cuenta invalida"}})
-		}
-	}).catch(async function(error){
-		await t.rollback();
-		console.log(error);							
-		res.json({"data":{"result":false,"message":"Cuenta de usuario no es valida"}})
-	})
+			res.json({"data":{"result":false,"message":"Cuenta de usuario no es valida"}})
+		})
+	}else{
+		res.redirect(process.env.HOST_FRONT+'expired/error')
+	}
 }
 async  function validateShop(req,res){
 	const {shopName}=req.params
