776f5c13eff62a8cd5efaa7f02fc397da00f9799
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-03-26 09:58:35 -0400

    fix login token

--- src/controllers/account.ctrl.js
+++ src/controllers/account.ctrl.js
@@ -497,32 +497,36 @@ async function loginToken(req,res){
 	const {token}= req.params	
 	//const token= req.header('Authorization').replace('Bearer ', '');
 	if(!token){
-		res.json({"result":false,"message":"Su token no es valido"});
-		res.redirect(process.env.HOST_FRONT+'expierd/error')
+		//res.json({"result":false,"message":"Su token no es valido"});
+		res.redirect(process.env.HOST_FRONT+'expired/error');
 	}else{
 
 		try{
 			
 			await generals.currentAccount(token)		
 			.then(async function(rsCurrentAccount){	
-				console.log(rsCurrentAccount);		
-				await generals.getShopId(token)
-				.then(async function(getShop){
-							console.log(rsCurrentAccount);
-					if(getShop){
-						res.json({"data":{"result":true,"message":"Usted a iniciado sesión como "+rsCurrentAccount['data'].account.email,
-							"account":{ "id": rsCurrentAccount['data'].account.id,"name":rsCurrentAccount['data'].account.name,"email":rsCurrentAccount['data'].account.email},
-							"role":rsCurrentAccount['data'].role,
-							"shop":{"id":getShop.id,"postulacionId":getShop.shopRequestId,"name":getShop.name}}}	)			
-					}else{
-						res.json({"data":{"result":true,"message":"Usted a iniciado sesión como "+rsCurrentAccount['data'].account.email,
-							"account":{ "id": rsCurrentAccount['data'].account.id,"name":rsCurrentAccount['data'].account.name,"email":rsCurrentAccount['data'].account.email},
-							"role":{"id":rsCurrentAccount['data'].role.id,"name":rsCurrentAccount['data'].role.name}}})			
-					}				
-				}).catch(async function(error){
-					console.log(error);
-					res.json({"data":{"result":false,"message":"Algo no salio bien, no se pudo buscar las tiendas"}})
-				})			
+				console.log(rsCurrentAccount);
+				if(rsCurrentAccount==false){
+					res.redirect(process.env.HOST_FRONT+'expired/error');
+				}else{
+					await generals.getShopId(token)
+					.then(async function(getShop){							
+						if(getShop){
+							res.json({"data":{"result":true,"message":"Usted a iniciado sesión como "+rsCurrentAccount['data'].account.email,
+								"account":{ "id": rsCurrentAccount['data'].account.id,"name":rsCurrentAccount['data'].account.name,"email":rsCurrentAccount['data'].account.email},
+								"role":rsCurrentAccount['data'].role,
+								"shop":{"id":getShop.id,"postulacionId":getShop.shopRequestId,"name":getShop.name}}}	)			
+						}else{
+							res.json({"data":{"result":true,"message":"Usted a iniciado sesión como "+rsCurrentAccount['data'].account.email,
+								"account":{ "id": rsCurrentAccount['data'].account.id,"name":rsCurrentAccount['data'].account.name,"email":rsCurrentAccount['data'].account.email},
+								"role":{"id":rsCurrentAccount['data'].role.id,"name":rsCurrentAccount['data'].role.name}}})			
+						}				
+					}).catch(async function(error){
+						console.log(error);
+						res.json({"data":{"result":false,"message":"Algo no salio bien, no se pudo buscar las tiendas"}})
+					})	
+				}
+						
 			}).catch(async function(error){
 				console.log(error);
 				res.json({"data":{"result":false,"message":"Su token no es valido"}})
