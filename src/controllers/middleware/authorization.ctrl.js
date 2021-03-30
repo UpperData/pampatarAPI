982ac5d60118cb809d2fbc246ebbd73d0a9ec920
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-03-30 11:30:05 -0400

    fix authorization

--- src/controllers/middleware/authorization.ctrl.js
+++ src/controllers/middleware/authorization.ctrl.js
@@ -13,43 +13,45 @@ var requireRole=function(roles){
             }else{
                 //   var token=req.headers.authorization.split(' ')[1];
                 const token = req.header('Authorization').replace('Bearer ', '');  
-                try{       
+                //try{       
+                if(token){
                     var payload= await jwt.decode(token,process.env.JWT_SECRET)
-                }catch(error){
+                /*}catch(error){
                     res.redirect(process.env.HOST_FRONT+"expired/error");                 
-                }
-                if(payload){
-                    if(payload.exp<=moment().unix()){
-                    
-                        //res.redirect(process.env.HOST_FRONT+"expired/error"); 
-                        res.status(401).json({"data":{"result":false,"message":"Su sesión a expirado, por favor incie sesión nuevamente"}})        
-                    }else if(payload.role.length<1){// permiso denegado con token valido
-                        //res.redirect(process.env.HOST_FRONT+"expired/error");
-                        res.status(401).json({"data":{"result":false,"message":"Su usuario tiene permiso para el realizar esta acción"}})              
-                    }else if(payload.rem!='lo-veremos-cara-a-cara'){ //No emitido por pampatar posible intento de haker
-                        res.status(401).json({"data":{"result":false,"message":"Origen de su identidad no es valido, por favor incice sesión nuevamente"}})        
-                        //res.redirect(process.env.HOST_FRONT+"expired/error");
-                    } else{
-                        //console.log(payload.role);
-                        var roleGroups=payload.role
-                        for (var i = 0; i < authorized.length; i++){
-                            for (var j = 0; j < roleGroups.length; j++){
-                                if (authorized[i].id == roleGroups[j].id){
-                                    isValid=true
+                }*/
+                    if(payload){
+                        if(payload.exp<=moment().unix()){
+                        
+                            //res.redirect(process.env.HOST_FRONT+"expired/error"); 
+                            res.status(401).json({"data":{"result":false,"message":"Su sesión a expirado, por favor incie sesión nuevamente"}})        
+                        }else if(payload.role.length<1){// permiso denegado con token valido
+                            //res.redirect(process.env.HOST_FRONT+"expired/error");
+                            res.status(401).json({"data":{"result":false,"message":"Su usuario tiene permiso para el realizar esta acción"}})              
+                        }else if(payload.rem!='lo-veremos-cara-a-cara'){ //No emitido por pampatar posible intento de haker
+                            res.status(401).json({"data":{"result":false,"message":"Origen de su identidad no es valido, por favor incice sesión nuevamente"}})        
+                            //res.redirect(process.env.HOST_FRONT+"expired/error");
+                        } else{
+                            //console.log(payload.role);
+                            var roleGroups=payload.role
+                            for (var i = 0; i < authorized.length; i++){
+                                for (var j = 0; j < roleGroups.length; j++){
+                                    if (authorized[i].id == roleGroups[j].id){
+                                        isValid=true
+                                    }
                                 }
-                            }
-                        } 
-                    }  
-                    if(!isValid){ // No autorizado ->posible intento de haker
-                        //res.redirect(process.env.HOST_FRONT+"expired/error");
-                        res.status(401).json({"data":{"result":false,"message":"No tiene autorización para acceder a este modulo"}})  
-                    }else{
-                        next();
+                            } 
+                        }  
+                        if(!isValid){ // No autorizado ->posible intento de haker
+                            //res.redirect(process.env.HOST_FRONT+"expired/error");
+                            res.status(401).json({"data":{"result":false,"message":"No tiene autorización para acceder a este modulo"}})  
+                        }else{
+                            next();
+                        }
+                    }else{//No emitido por pampartar ->posible intento de haker
+                        res.redirect(process.env.HOST_FRONT+"expired/error");
+                        // res.end()
+                        //res.status(401).json({"data":{"result":false,"message":"Token desconocido"}})  
                     }
-                }else{//No emitido por pampartar ->posible intento de haker
-                   //res.redirect(process.env.HOST_FRONT+"expired/error");
-                   res.end()
-                    //res.status(401).json({"data":{"result":false,"message":"Token desconocido"}})  
                 }
             } 
         }
