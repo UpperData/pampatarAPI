f67054ed8a9d8cd9de390634d8349456eb63bb30
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-03-31 10:38:47 -0400

    fix mail send

--- src/controllers/mail.ctrl.js
+++ src/controllers/mail.ctrl.js
@@ -4,27 +4,39 @@ exports.sendEmail = function(req, res){
     // Definimos el transporter
         var transporter = nodemailer.createTransport({
             //service: 'Gmail',
-           // pool: true,
-           // secure: true,
-            host: 'smtp.office365.com',
-            port: '587',
-            secureConnection: false,
+            pool: true,
+            secure: true,
+            host: 'smtp.gmail.com',
+            port: 465,
             //ignoreTLS: false,
             //secure: false,
             auth: {               
                 user: process.env.EMAIL_ADMIN,
                 pass: process.env.PASS_EMAIL_ADMIN                
-            },
-            tls: { ciphers: 'SSLv3' }
-            
-            /*host: 'smtp.gmail.com',
-            port: 465,
+            }
+            //Office 365 Modern SMTP
+            /*
+            pool: true,
             secure: true,
-            auth: {
-                type: 'OAuth2',
-                user: 'user@example.com',
-                accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
-            }*/
+            host: 'smtp.gmail.com',
+            port: 465,
+            //ignoreTLS: false,
+            //secure: false,
+            auth: {               
+                user: process.env.EMAIL_ADMIN,
+                pass: process.env.PASS_EMAIL_ADMIN                
+            }
+            */
+            // Office 360 OLD SMTP
+            /*host: 'smtp.office365.com',
+            port: '587',
+            secureConnection: false,
+            
+            auth: {               
+                user: process.env.EMAIL_ADMIN,
+                pass: process.env.PASS_EMAIL_ADMIN                
+            },
+            tls: { ciphers: 'SSLv3' }*/
         });
     // Definimos el email
     var mailOptions = {
