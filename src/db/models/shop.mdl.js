7a4a20d98e95d20773336ddecf84e346cb9c9f15
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-04-08 08:44:58 -0400

    fix shop model

--- src/db/models/shop.mdl.js
+++ src/db/models/shop.mdl.js
@@ -65,22 +65,20 @@ module.exports = (sequelize, DataTypes) => {
     	type:DataTypes.STRING,
       allowNull:true  
     },
-    statusProcessId: {
-      type:DataTypes.INTEGER,
-      allowNull:false,		
-      defaultValue:1
-    },
     statusId: {
       type:DataTypes.INTEGER,
       allowNull:false,		
-      defaultValue:1
+      defaultValue:1,
+      references:{
+        model:{tableName:'Status',schema:'public'},key:'id'
+      }
     }
   }, {});
   shop.associate = function(models) {
     shop.belongsTo(models.shopRequest);
-    shop.belongsTo(models.statusProcess);
-    shop.belongsTo(models.Status,{foreignkey:'statusId', as:'status'});
+    shop.belongsTo(models.Status, {foreignKey:'statusId'});
     shop.hasMany(models.Bids);
+    //shop.hasMany(models.Bids);
     //models.shopRequest.belongsTo(shop);
     // associations can be defined here
   };
