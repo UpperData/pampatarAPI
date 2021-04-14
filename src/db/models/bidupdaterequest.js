1d7817b865e6b2405acbba2ab7276f6ca6e16d76
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-04-14 11:38:47 -0400

    Bid update request fix

--- src/db/models/bidupdaterequest.js
+++ src/db/models/bidupdaterequest.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   bidUpdateRequest.associate = function(models) {
     // associations can be defined here
+    bidUpdateRequest.belongsTo(models.Bids);
   };
   return bidUpdateRequest;
 };
