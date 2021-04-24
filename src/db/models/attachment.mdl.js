b8e18154e9d1f380ee2f63a9faf7e47cd0dceaa6
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-04-23 21:29:39 -0400

    General Attachment Type Get fix

--- src/db/models/attachment.mdl.js
+++ src/db/models/attachment.mdl.js
@@ -1,16 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const attachment = sequelize.define('attachment', {
-    data: 
-    {type:DataTypes.STRING,
-    allowNull:false,
-    validate:{     
-      notEmpty:true        
-    }
-  },
+    data:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        notEmpty:true
+      }
+    },
     tags: {
       type:DataTypes.JSONB,
       allowNull:false,
+      validate:{     
+        notEmpty:true        
+      }
+    },attachmentTypeId:{
+      type:DataTypes.INTEGER,
+      allowNull:true,
       validate:{     
         notEmpty:true        
       }
