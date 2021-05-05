748fa9caf4f4614ee908bd21d9e72e2ace1e2bb9
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-05-05 08:52:06 -0400

    get img bid by id fix

--- src/controllers/generals.ctrl.js
+++ src/controllers/generals.ctrl.js
@@ -1022,7 +1022,7 @@ async function getImgByBid(req,res){
 		attributes:['id','photos'],
 		where:{id:bidId}	
 	}).then(async function (rsImg){
-		if(rsImg){
+		if(rsImg.photos[0].id){
 			console.log(rsImg.photos); 
 			var imgs=[];			
 			for (var i = 0; i < rsImg.photos.length; i++){ 
