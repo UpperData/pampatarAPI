ba1e9549ea2a27dcb607cd97a0d5c538d7683fed
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-08-23 10:03:45 -0400

    seller dashboard fix

--- src/db/migrations/20210707193127-updatePurchaseorder.js
+++ src/db/migrations/20210707193127-updatePurchaseorder.js
@@ -6,7 +6,13 @@ module.exports = {
           await queryInterface.addColumn('purchaseOrders', 'shopId', {
               type: Sequelize.INTEGER,
           });
-      }        
+      } 
+      if (!tableDefinition.pay) {
+          
+        await queryInterface.addColumn('purchaseOrders', 'pay', {
+            type: Sequelize.JSONB
+        });
+       }        
       await queryInterface.addConstraint('purchaseOrders',['shopId'], {
           type: 'FOREIGN KEY',
           name: 'shop_purchaseOrder_fk', // useful if using queryInterface.removeConstraint
