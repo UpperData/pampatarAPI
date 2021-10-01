cd0041762e634a9f5c2897bf6f942cb8f0ec080b
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2021-10-01 13:09:25 -0400

    login token

--- src/controllers/admin.ctrl.js
+++ src/controllers/admin.ctrl.js
@@ -2297,8 +2297,9 @@ async function getSalesdByMonthByShopAdmin(req,res){ // Ventas por mes Cantidad 
 	const{year,month}=req;
 	var startedDate= moment(new Date(year+"-"+month+"-"+'1')).format('YYYY-MM-DD');
 	const lastDay = moment(new Date(year+"-"+month)).daysInMonth(); // Dias del mes
-	var endDate = moment(startedDate).add(lastDay-1, 'days').calendar(); 
-	var endDate=moment(endDate).format('YYYY-MM-DD');
+	var endDate = moment(startedDate).add(lastDay-1, 'days'); 
+	//var endDate=moment(endDate).format('YYYY-MM-DD');
+	//if(lastDay=='Today'){endDate=momenttoday()}
 	console.log(`mes ${month} dias ${endDate}` )
 	let sTotalSales=0;
 	let pTotalSales=0;
