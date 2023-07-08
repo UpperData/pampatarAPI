6176231bf7179c857e06db5984dca34e978e97d8
Angel <upperdata@gmail.com>
2023-07-07 20:04:11 -0400

    migrate update

--- src/db/migrations/20210823000-create-account.js
+++ src/db/migrations/20210823000-create-account.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull:false,
         validate:{
           isAlpha:true,
-          len:[4,16],
+          len:[6,16],
           notEmpty:true
         }
       },
